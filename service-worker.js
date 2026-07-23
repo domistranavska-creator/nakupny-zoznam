3bccf629f8c5014b562a78caf8480e759c6a5fa5
domistranavska-creator <domistranavska@gmail.com>
2026-07-23 11:57:10 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nakupny-zoznam-runtime-v2026-07-23-packed-last";
+const CACHE_NAME = "nakupny-zoznam-runtime-v2026-07-23-reorder";
 const PRECACHE_ASSETS = [
   "./",
   "./index.html",
